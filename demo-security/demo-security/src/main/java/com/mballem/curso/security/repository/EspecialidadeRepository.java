32ae0fa1049a6753311a537b1a26f6c8528b7b66
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-11-19 01:41:55 -0300

    cadastrando especialidades

---
+++ demo-security/demo-security/src/main/java/com/mballem/curso/security/repository/EspecialidadeRepository.java
@@ -0,0 +1,7 @@
+package com.mballem.curso.security.repository;
+
+import com.mballem.curso.security.domain.Especialidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EspecialidadeRepository extends JpaRepository<Especialidade, Long> {
+}
