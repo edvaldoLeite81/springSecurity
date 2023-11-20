935442b974e5dce24724cdee1b3321881a1dc66f
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-11-19 22:27:24 -0300

    listando especialidades

--- demo-security/demo-security/src/main/java/com/mballem/curso/security/service/EspecialidadeService.java
+++ demo-security/demo-security/src/main/java/com/mballem/curso/security/service/EspecialidadeService.java
@@ -1,10 +1,16 @@
 package com.mballem.curso.security.service;
 
+import com.mballem.curso.security.datatables.Datatables;
+import com.mballem.curso.security.datatables.DatatablesColunas;
 import com.mballem.curso.security.domain.Especialidade;
 import com.mballem.curso.security.repository.EspecialidadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @Service
 public class EspecialidadeService {
@@ -12,8 +18,22 @@ public class EspecialidadeService {
     @Autowired
     private EspecialidadeRepository especialidadeRepository;
 
+    @Autowired
+    private Datatables datatables;
+
     @Transactional()
-    public void salvar(Especialidade especialidade){
+    public void salvar(Especialidade especialidade) {
         especialidadeRepository.save(especialidade);
+    }
+
+    @Transactional(readOnly = true)
+    public Map<String, Object> buscarEspecialidades(HttpServletRequest request) {
+       datatables.setRequest(request);
+       datatables.setColunas(DatatablesColunas.ESPECIALIDADES);
+       Page<?> page = datatables.getSearch().isEmpty()
+               ? especialidadeRepository.findAll(datatables.getPageable())
+               : especialidadeRepository.findAllByTitulo(datatables.getSearch(), datatables.getPageable());
+
+        return datatables.getResponse(page);
     }
 }
